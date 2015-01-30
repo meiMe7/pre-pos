121cb850b36cb37013bc66c0f6f68bb2eb1acb84
meiMe7 <959383478@qq.com>
2015-01-30 16:55:45 +0800

    practice_1_4完成

--- practices/section_1/practice_4/practice.js
+++ practices/section_1/practice_4/practice.js
@@ -1,15 +1,14 @@
 function collect_same_elements(collection_a, object_b) {
     //在这里写入代码
-
     var temp=[];
 
-    for(var i in collection_a['key']){
+    for(var index in collection_a){
 
         for(var j=0;j< object_b['value'].length;j++)
         {
 
-            if (collection_a['key'][i] ==  object_b['value'][j]) {
-                temp.push(collection_a['key'][i]);
+            if (collection_a[index].key==  object_b['value'][j]) {
+                temp.push(object_b['value'][j]);
 
             }
         }
