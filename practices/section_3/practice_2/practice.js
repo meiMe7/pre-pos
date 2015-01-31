9af2fc882066921c7f460c586854669375f5af64
meiMe7 <959383478@qq.com>
2015-01-31 15:26:08 +0800

    完成3-2

--- practices/section_3/practice_2/practice.js
+++ practices/section_3/practice_2/practice.js
@@ -1,6 +1,25 @@
 function create_updated_collection(collection_a, object_b)
 {
     //在这里写入代码
+
+    for(var i=0;i<collection_a.length;i++){
+
+        for(var j=0;j< object_b['value'].length;j++)
+        {
+            var temp=collection_a[i].count-3;
+            if (collection_a[i].key ==  object_b['value'][j]) {
+
+
+                while(temp >= 0){
+                    collection_a[i].count--;
+                    temp -=3;
+                }
+
+            }
+        }
+
+    }
+    return collection_a;
 }
 
 module.exports = create_updated_collection;
